28e54c43456f0813dc8f4e0bc55815fbb2b9865c
Kruithne <kruithne@gmail.com>
2013-09-17 00:23:44 +0100

    Initial set-up of all the things.

--- src/no/runsafe/provinggrounds/Plugin.java
+++ src/no/runsafe/provinggrounds/Plugin.java
@@ -7,6 +7,6 @@ public class Plugin extends RunsafePlugin
 	@Override
 	protected void PluginSetup()
 	{
-		//addComponent(SomeComponent.class);
+		// Note from JettKuso: The trial is complete. We have observed all of you and gathered what information we needed. (winner), we shall grant you a small prize for your efforts, but this is only the first test.
 	}
 }

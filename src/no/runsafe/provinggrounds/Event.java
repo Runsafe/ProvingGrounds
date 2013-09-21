67bd5c0040e6835190960cac3bc4f6fb3fbdb499
Kruithne <kruithne@gmail.com>
2013-09-21 22:58:50 +0100

    Debug?!

--- src/no/runsafe/provinggrounds/Event.java
+++ src/no/runsafe/provinggrounds/Event.java
@@ -83,6 +83,7 @@ public class Event implements IConfigurationChanged, IPlayerTeleport, IPlayerMov
 	@Override
 	public boolean OnPlayerRightClick(RunsafePlayer player, RunsafeMeta usingItem, RunsafeBlock block)
 	{
+		console.fine("We detected a right click event");
 		if (block != null && block.is(Item.Decoration.Head.Skeleton))
 		{
 			console.fine("Detected right click on a skull");

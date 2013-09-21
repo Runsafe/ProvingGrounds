7cd3bdcf2995634ae46fbda11eb283065f5088ff
Kruithne <kruithne@gmail.com>
2013-09-21 22:10:42 +0100

    Updated to use INT over TINYINT, prevent nulls?

--- src/no/runsafe/provinggrounds/SkullsRepository.java
+++ src/no/runsafe/provinggrounds/SkullsRepository.java
@@ -58,7 +58,7 @@ public class SkullsRepository extends Repository
 						"`x` INT(10) NOT NULL," +
 						"`y` INT(10) NOT NULL," +
 						"`z` INT(10) NOT NULL," +
-						"`looted` TINYINT(1) NOT NULL DEFAULT 0" +
+						"`looted` INT(1) NOT NULL DEFAULT 0" +
 					")"
 		);
 		queries.put(1, sql);
